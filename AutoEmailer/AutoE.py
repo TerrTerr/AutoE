9369657fb904c883c1523f838e27457bffd097d3
TerrTerr <139302821+TerrTerr@users.noreply.github.com>
2023-07-29 14:11:06 -0400

    Update AutoE.py

--- AutoEmailer/AutoE.py
+++ AutoEmailer/AutoE.py
@@ -5,39 +5,39 @@ import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# Your Gmail account details
+#Your Gmail account 
 gmail_user = 'your-email@gmail.com'
 gmail_password = 'your-password'
 
-# List of recipients
+#List of recipients
 with open('recipients.csv', 'r') as file:
     reader = csv.DictReader(file)
     recipients = list(reader)
 
-# Email subject
+#Email subject
 subject = 'Your Subject Here'
 
-# Connect to Gmail
+#Connect to Gmail
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(gmail_user, gmail_password)
 
-# Loop through each recipient and send the email
+#Loop through each recipient and send the email
 for recipient in recipients:
     msg = MIMEMultipart()
     msg['From'] = gmail_user
     msg['To'] = recipient['email']
     msg['Subject'] = subject
 
-    # Email body
+    #Email body
     body = 'Dear {},<br><br>Your email body here.'.format(recipient['name'])
     msg.attach(MIMEText(body, 'html'))
 
-    # Send the email
+    #Send email
     server.send_message(msg)
 
-    # Wait for 100 seconds
+    #Wait 100 seconds
     time.sleep(100)
 
-# Close the connection to the email server
+#Close the connection to the email server
 server.quit()
